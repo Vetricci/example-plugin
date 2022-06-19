f4a9bea765dfe49cc841388536957cf6cc9f350c
Vetricci <81270549+Vetricci@users.noreply.github.com>
2022-06-19 16:24:00 -0500

    Initial

--- src/test/java/com/Neverlog/NeverlogPluginTest.java
+++ src/test/java/com/Neverlog/NeverlogPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.Neverlog;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class NeverlogPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(NeverlogPlugin.class);
 		RuneLite.main(args);
 	}
 }
